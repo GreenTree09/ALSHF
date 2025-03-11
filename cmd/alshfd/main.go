a4330808bb5c72f30d7ca77aea265edfa151e55a
Green Tree <126562583+GreenTree09@users.noreply.github.com>
2025-03-11 03:30:59 -0500

    Update main.go

--- cmd/alshfd/main.go
+++ cmd/alshfd/main.go
@@ -6,32 +6,38 @@ import (
 	"internal/core/monitor"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	// Setup Firewall
 	if err := hardening.SetupFirewall("configs/hardening/firewall.yaml"); err != nil {
 		log.Fatalf("Error setting up firewall: %v", err)
+		os.Exit(1) // Exit the program on error
 	}
 
 	// Configure SELinux
 	if err := hardening.ConfigureSELinux("configs/hardening/selinux.yaml"); err != nil {
 		log.Fatalf("Error configuring SELinux: %v", err)
+		os.Exit(1) // Exit the program on error
 	}
 
 	// Setup AIDE
 	if err := monitor.SetupAIDE("configs/monitoring/aide.yaml"); err != nil {
 		log.Fatalf("Error setting up AIDE: %v", err)
+		os.Exit(1) // Exit the program on error
 	}
 
 	// Check Integrity
 	if err := monitor.CheckIntegrity(); err != nil {
 		log.Fatalf("Error checking integrity: %v", err)
+		os.Exit(1) // Exit the program on error
 	}
 
 	// Setup Tripwire
 	if err := monitor.SetupTripwire(); err != nil {
 		log.Fatalf("Error setting up Tripwire: %v", err)
+		os.Exit(1) // Exit the program on error
 	}
 
 	// Start HTTP server for monitoring
